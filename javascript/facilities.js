dce9df84b5e1343f6182512ac41b78bf2f6ca8d0
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2021-01-24 23:57:01 +0530

    Added facilities

--- javascript/facilities.js
+++ javascript/facilities.js
@@ -1,35 +1,152 @@
-var data = [
+var fabricationData = [
   {
-    name: "Ice Machine",
-    info: "Ice Machine Information",
-    imgLink: "./images/1.jpg",
+    name: "Micropatterning (Lithography)",
+    info: "Micropatterning (Lithography) Information",
+    imgLink: "./images/facility/fabrication/micropatterning.jpg",
   },
   {
-    name: "-30&#176; C Freezer",
-    info: "-30&#176; C Freezer Information",
-    imgLink: "./images/2.jpg",
+    name: "Electrospinning",
+    info: "Electrospinning Information",
+    imgLink: "./images/facility/fabrication/electrospinning.jpg",
   },
   {
-    name: "Chemical fume hood",
-    info: "Chemical fume hood Information",
-    imgLink: "./images/3.jpg",
+    name: "Coaters (Dip Coater with Heater, Regular Dip Coater)",
+    info: "Coaters (Dip Coater with Heater, Regular Dip Coater) Information",
+    imgLink: "./images/facility/fabrication/coaters.jpg",
+  },
+  {
+    name: "Spinner System",
+    info: "Spinner System Information",
+    imgLink: "./images/facility/fabrication/spinnerSystem.jpg",
+  },
+  {
+    name: "Convection Ovens",
+    info: "Convection Ovens Information",
+    imgLink: "./images/facility/fabrication/convectionOven.jpg",
+  },
+  {
+    name: "Plasma Cleaner and Vacuum Pump, Flow Meter System",
+    info: "Plasma Cleaner and Vacuum Pump, Flow Meter System Information",
+    // imgLink: "./images/facility/fabrication/.jpg",
+  },
+  {
+    name: "E-Beam Evaporator",
+    info: "E-Beam Evaporator Information",
+    // imgLink: "./images/facility/fabrication/.jpg",
+  },
+  {
+    name: "Pyrolysis Furnace",
+    info: "Pyrolysis Furnace Information",
+    // imgLink: "./images/facility/fabrication/.jpg",
+  },
+  {
+    name: "Oxidation Furnace",
+    info: "Oxidation Furnace Information",
+    imgLink: "./images/facility/fabrication/oxidationFurnace.jpg",
   },
 ];
-var table = document.getElementById("table-body");
-for (var i = 0; i < data.length; i++) {
-  var row = `
+var macData = [
+  {
+    name: "Raman-cum-PL setup",
+    info: "Raman-cum-PL setup",
+    imgLink: "./images/facility/mac/ramanCup.jpg",
+  },
+  {
+    name: "Rheometer",
+    info: "Rheometer Information",
+    imgLink: "./images/facility/mac/rheometer.jpg",
+  },
+  {
+    name: "AFM",
+    info: "AFM Information",
+    imgLink: "./images/facility/mac/AFM.jpg",
+  },
+  {
+    name: "Potentiostat",
+    info: "Potentiostat Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+  {
+    name: "Goniometer cum Optical Tensiometer",
+    info: "Goniometer cum Optical Tensiometer Information",
+    imgLink: "./images/facility/mac/goniometer.jpg",
+  },
+  {
+    name:
+      "Dynamic Light Scattering Particle Size Analyzer + Zeta Potential Meter",
+    info:
+      "Dynamic Light Scattering Particle Size Analyzer + Zeta Potential Meter Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+  {
+    name: "Force Tensiometer",
+    info: "Force Tensiometer Information",
+    imgLink: "./images/facility/mac/forceTensiometer.jpg",
+  },
+  {
+    name: "Semiconductor Parametric Analyser",
+    info: "Semiconductor Parametric Analyser Information",
+    // imgLink: "./images/facility/mac/forceTensiometer.jpg",
+  },
+  {
+    name: "Ellipsometer",
+    info: "Ellipsometer Information",
+    imgLink: "./images/facility/mac/ellipsometer.jpg",
+  },
+  {
+    name: "DAQ",
+    info: "DAQ Information",
+    // imgLink: "./images/facility/mac/ellipsometer.jpg",
+  },
+  {
+    name: "DC/RF Probe station",
+    info: "DC/RF Probe station Information",
+    imgLink: "./images/facility/mac/probeStation.jpg",
+  },
+  {
+    name: "XRD",
+    info: "XRD Information",
+    imgLink: "./images/facility/mac/xrd.jpg",
+  },
+  {
+    name: "FESEM",
+    info: "FESEM Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+];
+var softwareData = [
+  {
+    name: "VASP",
+    info: "VASP Information",
+    // imgLink: "./images/facility/software/.jpg",
+  },
+  {
+    name: "Gaussian",
+    info: "Gaussian Information",
+    // imgLink: "./images/facility/software/.jpg",
+  },
+];
+
+buildFabricationTable(fabricationData);
+buildMaCTable(macData);
+buildSoftwareTable(softwareData);
+
+function buildFabricationTable(fabData) {
+  var fabTable = document.getElementsByClassName("fabrication-table-body")[0];
+  for (var i = 0; i < fabData.length; i++) {
+    var row = `
   <tr>
         <td class="facility-text">${i + 1}</td>
         <td
           class="facility-name facility-text"
           data-toggle="modal"
-          data-target=${"#facility" + i}
+          data-target=${"#fabrication" + i}
         >
-          ${data[i].name}
+          ${fabData[i].name}
         </td>
         <div
           class="modal fade"
-          id=${"facility" + i}
+          id=${"fabrication" + i}
           tabindex="-1"
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
@@ -41,7 +158,7 @@ for (var i = 0; i < data.length; i++) {
                   class="modal-title facility-text"
                   id="exampleModalLabel"
                 >
-                ${data[i].name}
+                ${fabData[i].name}
                 </h5>
                 <button
                   type="button"
@@ -52,18 +169,129 @@ for (var i = 0; i < data.length; i++) {
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body facility-text">${data[i].info}</div>
+              <div class="modal-body facility-text">${fabData[i].info}</div>
             </div>
           </div>
         </div>
         <td>
           <img
             class="facility-img"
-            src=${data[i].imgLink}
+            src=${fabData[i].imgLink}
             alt=""
           />
         </td>
       </tr>
   `;
-  table.innerHTML += row;
+    fabTable.innerHTML += row;
+  }
+}
+function buildMaCTable(macData) {
+  var macTable = document.getElementsByClassName("mac-table-body")[0];
+  for (var i = 0; i < macData.length; i++) {
+    var row = `
+  <tr>
+        <td class="facility-text">${i + 1}</td>
+        <td
+          class="facility-name facility-text"
+          data-toggle="modal"
+          data-target=${"#mac" + i}
+        >
+          ${macData[i].name}
+        </td>
+        <div
+          class="modal fade"
+          id=${"mac" + i}
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5
+                  class="modal-title facility-text"
+                  id="exampleModalLabel"
+                >
+                ${macData[i].name}
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body facility-text">${macData[i].info}</div>
+            </div>
+          </div>
+        </div>
+        <td>
+          <img
+            class="facility-img"
+            src=${macData[i].imgLink}
+            alt=""
+          />
+        </td>
+      </tr>
+  `;
+    macTable.innerHTML += row;
+  }
+}
+function buildSoftwareTable(softwareData) {
+  var softwareTable = document.getElementsByClassName("software-table-body")[0];
+  for (var i = 0; i < softwareData.length; i++) {
+    var row = `
+  <tr>
+        <td class="facility-text">${i + 1}</td>
+        <td
+          class="facility-name facility-text"
+          data-toggle="modal"
+          data-target=${"#software" + i}
+        >
+          ${softwareData[i].name}
+        </td>
+        <div
+          class="modal fade"
+          id=${"software" + i}
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5
+                  class="modal-title facility-text"
+                  id="exampleModalLabel"
+                >
+                ${softwareData[i].name}
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body facility-text">${
+                softwareData[i].info
+              }</div>
+            </div>
+          </div>
+        </div>
+        <td>
+          <img
+            class="facility-img"
+            src=${softwareData[i].imgLink}
+            alt=""
+          />
+        </td>
+      </tr>
+  `;
+    softwareTable.innerHTML += row;
+  }
 }
