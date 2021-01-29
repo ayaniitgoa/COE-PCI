7cab7c60ab9330bd021708c2ff750bebd9ebebe1
Ayan Adhya <ayan.adhya.18003@iitgoa.ac.in>
2021-01-29 23:16:53 +0530

    Added recent publications

--- javascript/contactus.js
+++ javascript/contactus.js
@@ -44,6 +44,83 @@ var data = [
     info: "Oxidation Furnace Information",
     imgLink: "./images/facility/fabrication/oxidationFurnace.jpg",
   },
+  {
+    name: "Raman-cum-PL setup",
+    info: "Raman-cum-PL setup",
+    imgLink: "./images/facility/mac/ramanCup.jpg",
+  },
+  {
+    name: "Rheometer",
+    info: "Rheometer Information",
+    imgLink: "./images/facility/mac/rheometer.jpg",
+  },
+  {
+    name: "AFM",
+    info: "AFM Information",
+    imgLink: "./images/facility/mac/AFM.jpg",
+  },
+  {
+    name: "Potentiostat",
+    info: "Potentiostat Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+  {
+    name: "Goniometer cum Optical Tensiometer",
+    info: "Goniometer cum Optical Tensiometer Information",
+    imgLink: "./images/facility/mac/goniometer.jpg",
+  },
+  {
+    name:
+      "Dynamic Light Scattering Particle Size Analyzer + Zeta Potential Meter",
+    info:
+      "Dynamic Light Scattering Particle Size Analyzer + Zeta Potential Meter Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+  {
+    name: "Force Tensiometer",
+    info: "Force Tensiometer Information",
+    imgLink: "./images/facility/mac/forceTensiometer.jpg",
+  },
+  {
+    name: "Semiconductor Parametric Analyser",
+    info: "Semiconductor Parametric Analyser Information",
+    // imgLink: "./images/facility/mac/forceTensiometer.jpg",
+  },
+  {
+    name: "Ellipsometer",
+    info: "Ellipsometer Information",
+    imgLink: "./images/facility/mac/ellipsometer.jpg",
+  },
+  {
+    name: "DAQ",
+    info: "DAQ Information",
+    // imgLink: "./images/facility/mac/ellipsometer.jpg",
+  },
+  {
+    name: "DC/RF Probe station",
+    info: "DC/RF Probe station Information",
+    imgLink: "./images/facility/mac/probeStation.jpg",
+  },
+  {
+    name: "XRD",
+    info: "XRD Information",
+    imgLink: "./images/facility/mac/xrd.jpg",
+  },
+  {
+    name: "FESEM",
+    info: "FESEM Information",
+    // imgLink: "./images/facility/mac/.jpg",
+  },
+  {
+    name: "VASP",
+    info: "VASP Information",
+    // imgLink: "./images/facility/software/.jpg",
+  },
+  {
+    name: "Gaussian",
+    info: "Gaussian Information",
+    // imgLink: "./images/facility/software/.jpg",
+  },
 ];
 
 var selectDiv = document.getElementById("selectFacility");
@@ -53,5 +130,3 @@ for (var i = 0; i < data.length; i++) {
     `;
   selectDiv.innerHTML += row;
 }
-
-
